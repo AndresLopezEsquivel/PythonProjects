2ec31ba6ebc8320e263045652ec651a8f87c54eb
Andrés López <saurioandres456@gmail.com>
2021-05-24 11:25:51 -0500

    NATO Alphabet error handling implementation

--- NATO Alphabet/main.py
+++ NATO Alphabet/main.py
@@ -8,6 +8,17 @@ NATO_PHONETIC_ALPHABET_FILE_PATH = "nato_phonetic_alphabet.csv"
 
 nato_data = p.read_csv(NATO_PHONETIC_ALPHABET_FILE_PATH)
 nato_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
-word = input("Enter a word: ")
-nato_equivalent = [nato_dict[letter.upper()] for letter in word]
-print(nato_equivalent)
+
+continues_asking = True
+
+while continues_asking:
+    word = input("Enter a word: ")
+    try:
+        nato_equivalent = [nato_dict[letter.upper()] for letter in word]
+    except KeyError:
+        print("Sorry, only letters are allowed.")
+        print("Try again, please.")
+        print("\n")
+    else:
+        print(nato_equivalent)
+        continues_asking = False
