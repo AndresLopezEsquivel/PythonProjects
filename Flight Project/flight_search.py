1375637d9df39e0400187192a83eafd40738f094
Andrés López <saurioandres456@gmail.com>
2021-06-10 23:02:54 -0500

    Flight Project - Creating classes for flight data and flight searching

--- Flight Project/flight_search.py
+++ Flight Project/flight_search.py
@@ -1,19 +1,57 @@
 import os
 import requests
+from flight_data import FlightData
 
 TEQUILA_API_KEY = os.environ.get("TEQUILA_API_KEY")
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
 
 
+def get_data(endpoint, body, headers):
+    response = requests.get(url=endpoint,
+                            params=body,
+                            headers=headers)
+    return response.json()
+
+
 class FlightSearch:
 
     def get_destination_code(self, city_name: str) -> str:
-        headers = {"apiKey": TEQUILA_API_KEY}
         body = {"term": city_name}
         endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
-        response = requests.get(url=endpoint,
-                            params=body,
-                            headers=headers)
-        data = response.json()
+        headers = {"apiKey": TEQUILA_API_KEY}
+        data = get_data(endpoint=endpoint,
+                        body=body,
+                        headers=headers)
         iata_code = data["locations"][0]["code"]
         return iata_code
+
+    def check_for_flights(self, origin_city_code: str,
+                          destination_city_code: str,
+                          from_time: str,
+                          to_time: str,
+                          min_nights_in_dst: int,
+                          max_nights_in_dst: int):
+        endpoint = f"{TEQUILA_ENDPOINT}/v2/search"
+        body_request = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time,
+            "date_to": to_time,
+            "nights_in_dst_from": min_nights_in_dst,
+            "nights_in_dst_to": max_nights_in_dst
+        }
+        headers = {"apiKey": TEQUILA_API_KEY}
+        response = get_data(endpoint=endpoint,
+                            body=body_request,
+                            headers=headers)
+        data = response["data"][0]
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        return flight_data
--- Flight Project/main.py
+++ Flight Project/main.py
@@ -1,12 +1,19 @@
 from data_manager import DataManager
+from datetime import datetime, timedelta
 from flight_search import FlightSearch
 from pprint import pprint
 import os
 import requests
 
+ORIGIN_IATA_CODE = "LON"
+MIN_NIGHTS_IN_DST = 7
+MAX_NIGHTS_IN_DST = 28
+
 sheet_manager = DataManager()
 flight_search_manager = FlightSearch()
 sheet_data = sheet_manager.get_destination_data()
+
+print(sheet_data)
 
 for city in sheet_data:
     if not city["iataCode"]:
@@ -15,54 +22,19 @@ for city in sheet_data:
         city_iata_code = city["iataCode"]
         sheet_manager.update_city_iata_code(city_id=city_id,
                                             iata_code=city_iata_code)
+    origin_city_code = ORIGIN_IATA_CODE
+    destination_city_code = city["iataCode"]
+    now = datetime.now()
+    from_time = now.date().strftime("%d/%m/%Y")
+    to_time = (now.date() + timedelta(days=180)).strftime("%d/%m/%Y")
+    min_nights_in_dst = MIN_NIGHTS_IN_DST
+    max_nights_in_dst = MAX_NIGHTS_IN_DST
+    flight_data = flight_search_manager.check_for_flights(origin_city_code=origin_city_code,
+                                                          destination_city_code=destination_city_code,
+                                                          from_time=from_time,
+                                                          to_time=to_time,
+                                                          min_nights_in_dst=min_nights_in_dst,
+                                                          max_nights_in_dst=max_nights_in_dst)
+    print(f"{flight_data.destination_city}: £{flight_data.price}")
 
 sheet_manager.set_destination_data(destination_data=sheet_data)
-
-"""
-iata_codes = ["PAR"]
-
-headers_request = {
-    "apiKey": ""
-}
-
-endpoint = "https://tequila-api.kiwi.com/v2/search"
-
-date_from = datetime.now().date().strftime("%d/%m/%Y")
-
-date_to = (datetime.now().date() + timedelta(days=180)).strftime("%d/%m/%Y")
-
-nights_in_dst_from = 7
-
-nights_in_dst_to = 28
-
-for iata_code in iata_codes:
-    body_request = {
-        "fly_from": "LON",
-        "fly_to": iata_code,
-        "date_from": date_from,
-        "date_to": date_to,
-        "nights_in_dst_from": nights_in_dst_from,
-        "nights_in_dst_to": nights_in_dst_to
-    }
-    response = requests.get(url=endpoint,
-                            params=body_request,
-                            headers=headers_request)
-
-    data = response.json()["data"][0]
-
-    pprint.pprint(data)
-
-    price = f"{data['price']} €"
-
-    from_date = data["local_arrival"].split("T")[0]
-
-    date = from_date.split("-")
-
-    to_date = datetime(year=int(date[0]), month=int(date[1]), day=int(date[2])) + timedelta(days=int(data["nightsInDest"]))
-
-    print(f"Price: {price}")
-
-    print(f"From: {from_date}")
-
-    print(f"To: {to_date.date()}")
-"""
