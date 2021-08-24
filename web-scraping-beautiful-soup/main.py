64e6963d63734f0b4591576e58e3a27b98e1c5d8
Andrés López <saurioandres456@gmail.com>
2021-08-24 13:26:07 -0500

    Web scraping with Beautiful Soup

--- web-scraping-beautiful-soup/main.py
+++ web-scraping-beautiful-soup/main.py
@@ -21,7 +21,7 @@ print(f"Website's title content: {soup.title.string}")
 
 # Using  find_all() function
 
-print("\n == ANCHOR TAGS == \n")
+print("\n == ANCHOR TAGS ==")
 
 all_anchor_tags = soup.find_all(name="a")
 
@@ -30,3 +30,41 @@ for index in range(len(all_anchor_tags)):
     print(all_anchor_tags[index])
     print(all_anchor_tags[index].getText())
     print(all_anchor_tags[index].get("href"))
+
+# Using find() function
+
+heading_with_id = soup.find(name="h1", id="name")
+
+print("\n == GETTING HEADING THROUGH ITS ID ==")
+
+print(heading_with_id)
+
+print(f"Heading's text: {heading_with_id.getText()}")
+
+print("\n == GETTING HEADING THROUGH ITS CLASS ==")
+
+heading_with_class = soup.find(name="h3", class_="heading")
+
+print(heading_with_class)
+
+print(heading_with_class.name)
+
+print(heading_with_class.getText())
+
+print(heading_with_class.get("class"))
+
+# Using select_one() function
+
+print("\n == USING select_one() FUNCTION ==")
+
+first_anchor_tag = soup.select_one(selector="p a")
+
+print(first_anchor_tag)
+
+# Using select() function
+
+print("\n == USING select() FUNCTION ==")
+
+elements_with_heading_class = soup.select(selector=".heading")
+
+print(elements_with_heading_class)
